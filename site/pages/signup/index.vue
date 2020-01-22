06119d54f9f3e73fa8f7ec01f2c3b3c34ced1671
Jorge Ramirez <jramirez@novopayment.com>
2020-01-22 18:13:26 -0400

    Crea vista para registro de usuario

---
+++ site/pages/signup/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2 class="title">Registro de usuario</h2>
+    <form>
+      <div>
+        <label>Usuario</label>
+        <input type="text">
+      </div>
+      <div>
+        <label>Contraseña</label>
+        <input type="password">
+      </div>
+      <input type="submit">
+    </form>
+  </div>
+</template>
