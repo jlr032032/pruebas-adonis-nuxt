2eddefeb7cd141ec4cfc672143341313970af124
Jorge Ramirez <jramirez@novopayment.com>
2020-01-22 16:05:05 -0400

    Agrega página inicial para login

--- site/components/AppMenu.vue
+++ site/components/AppMenu.vue
@@ -1,6 +1,18 @@
 <template>
   <nav>
-    <nuxt-link to="/">Inicio</nuxt-link>
+    <nuxt-link to="/" tag="li">Inicio</nuxt-link>
+    <nuxt-link to="/login" tag="li">Login</nuxt-link>
   </nav>
 </template>
 
+<style>
+  li{
+    list-style: none;
+    text-decoration: none;
+    color: #007bff
+  }
+  li:hover{
+    text-decoration: underline;
+    cursor: pointer
+  }
+</style>
