5dff2f14fc47ad791c1a9c3088d977129522d70e
Jorge Ramirez <jramirez@novopayment.com>
2020-01-23 11:14:46 -0400

    Añade funcionalidad de inicio de sesión

--- api/app/Controllers/Http/LoginController.js
+++ api/app/Controllers/Http/LoginController.js
@@ -1,8 +1,26 @@
 'use strict'
 
+const User = use('App/Models/User')
+
 class LoginController {
 
-  login({request}){
+  async login({request, auth, response}){
+    try {
+      const token = await auth.attempt(
+        request.input('username'),
+        request.input('password')
+      )
+      return response.json({
+        status: 'success',
+        data: token
+      })
+    } catch (error) {
+      console.log(error)
+      response.status(500).json({
+        status: 'error',
+        message: 'Combinación incorrecta de usuario y contraseña'
+      })
+    }
     return request.post()
   }
 
