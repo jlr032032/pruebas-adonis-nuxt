c50b2d2cd321660b9a5e6e391d25d4120b2b4df8
Jorge Ramirez <jramirez@novopayment.com>
2020-02-10 18:50:07 -0400

    Registra proveedor de antl

--- api/start/app.js
+++ api/start/app.js
@@ -18,6 +18,7 @@ const providers = [
   '@adonisjs/bodyparser/providers/BodyParserProvider',
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/lucid/providers/LucidProvider',
+  '@adonisjs/antl/providers/AntlProvider',
   path.join(__dirname, '..', 'providers', 'Encrypter/Provider'),
   path.join(__dirname, '..', 'providers', 'Requester/Provider')
 ]
