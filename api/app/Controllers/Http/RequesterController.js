37625ccfcb30f5dbadcd068eabbc325186819513
Jorge Ramirez <jramirez@novopayment.com>
2020-02-10 16:25:51 -0400

    Incorpora simulación de comunicación con API de backend

--- api/app/Controllers/Http/RequesterController.js
+++ api/app/Controllers/Http/RequesterController.js
@@ -1,10 +1,19 @@
 'use strict'
 
+const requester = use('Custom/Requester')
+
 class RequesterController {
 
-  index({request, response}){
-    const {method, status} = request.get()
-    response.json({method, status})
+  async index({request, response}){
+    const {status} = request.get()
+    try {
+      const result = await requester.get(`http://localhost:3334/backend-api/response?status=${status}`)
+      logger.debug(`Request result: ${JSON.stringify(result)}`)
+      return response.json(result)
+    } catch (error) {
+      logger.error(error.stack)
+      return { error: 'error' }
+    }
   }
 
 }
--- fake-backend-api/app/Controllers/Http/ResponseSimulatorController.js
+++ fake-backend-api/app/Controllers/Http/ResponseSimulatorController.js
@@ -4,6 +4,7 @@ class ResponseSimulatorController {
 
   index({request, response}){
     const { status } = request.get()
+    console.log(`Petición de respuesta con código HTTP ${status}`)
     const reqBody = { message: 'Here goes server feedback' }
     if(status)
       response.status(status)
